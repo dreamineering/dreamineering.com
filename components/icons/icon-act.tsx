11d5954d1a60d59aa38ab1aaadddddcd4935e2cd
Matt Mischewski <matt@dreamineering.com>
2021-10-08 13:02:45 +0200

    add logo icons

---
+++ components/icons/icon-act.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const IconAct = ({}) => {
+  return (
+    <svg
+      viewBox="0 0 200 200"
+      fill="#B91C1C"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M0 200V0L200 100L0 200Z" />
+    </svg>
+  );
+};
